52dbb9f1f30a908d67b66406c25f22a401893ac7
andres <andres@andres-Lenovo-G580>
2020-10-07 13:13:22 -0300

    Checkeo de tipos y rango

--- tp_compiladores/src/analizador_lexico/LlegaTokenValido.java
+++ tp_compiladores/src/analizador_lexico/LlegaTokenValido.java
@@ -21,7 +21,8 @@ public class LlegaTokenValido extends AccionSemantica {
 		
 		System.out.println("AS3 -> Voy a Ef con un Token Valido ->> "+ super.buffer + "	, tipo ->"+super.tipo_buffer);
 		
-		//aca checkeo rango -> hago AS 3.1 , 3.2 
+		//aca checkeo tipos y rango de un token valido -> hago AS 3.1 , 3.2 y 3.3
+		//if super.tipo == "ID" -> checkeo rango
 		//if (super.tipo == "INTEGER") -> checkeo tipos de enteros y trunco si es necesario
 		//if (super.tipo == "DOUBLE") -> checkeo tipos de enteros y si es necesario trunco
 
